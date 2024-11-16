51bdca508e238722f84ae4889e4cb4a4cecc3f74
mp022 <mpferreiradesouza@gmail.com>
2024-11-16 18:41:56 -0300

    feat: Adição de um metodo auxiliar que será usado na implementação da pesquisa, inserção e remoção

--- src/trieCompacta.py
+++ src/trieCompacta.py
@@ -8,7 +8,7 @@ class TrieCompacta:
     def __init__(self) -> None:
         self.raiz = No()
 
-    def maiorPrefixo(self, a: str, b: str):
+    def maiorPrefixo(self, a: str, b: str) -> int:
         r = -1
         # Tamanho da menor palavra recebida
         rep = min(a.__len__(), b.__len__())
@@ -18,10 +18,12 @@ class TrieCompacta:
             r = i
         return r+1
 
-    def pesquisar(self, chave: str):
+    # Retorna um valor i que indica quantas letras da chave foram processadas e uma lista com os indices dos nós partindo da raiz até o nó que possui o valor da chave pesquisada ou deveria possuir o valor da chave (ou seja, a chave pesquisada é menor ou igual a chave do nó) ou o nó pai mais proximo ao nó filho que deveria possuir o valor da chave
+    def pesquisaCaminho(self, chave: str) -> tuple[list[int], int]:
         noPai = self.raiz
         # letra atual da chave pesquisada
-        i = 0
+        i = ri = 0
+        r = []
         while noPai.filhos.__len__() > 0 and i < chave.__len__():
             for f in range(0, noPai.filhos.__len__()):
                 noAtual = noPai.filhos[f]
@@ -30,12 +32,14 @@ class TrieCompacta:
 
                 if maiorPrefixoComum == 0:
                     if f+1 == noPai.filhos.__len__():
-                        return -1
+                        return (r, ri)
                     continue
 
-                if  chave.__len__() - i < noAtual.chave.__len__() :
+                r.append(f)
+                ri += maiorPrefixoComum
+                if  chave.__len__() - i < noAtual.chave.__len__():
                     if maiorPrefixoComum > 0:
-                        return -1
+                        return (r, ri)
                     continue
                 elif noAtual.chave.__len__() <  chave.__len__() - i:
                     if noAtual.chave.__len__() == maiorPrefixoComum:
@@ -45,29 +49,45 @@ class TrieCompacta:
                     else:
                         # Chave do nó já foi toda processada
                         # Mas não bateu completamente com a chave
-                        return -1
+                        return (r, ri)
                 else:
                     if chave.__len__() - i == maiorPrefixoComum:
                         if noAtual.valor != -1:
-                            return noAtual.valor
+                            return (r, ri)
                         else:
                             # Chave já foi toda processada
                             # Verifica se exite nó filho sem chave
                             if (noAtual.filhos.__len__() > 0) and noAtual.filhos[0].chave == '':
+                                r.append(0)
                                 # Apenas nós folha possuem valor
-                                return noAtual.filhos[0].valor
+                                return (r, ri)
                             else:
                                 # Não possui valor no nó encontrado
-                                return -1
+                                return (r, ri)
                     else:
                         # Chave já foi toda processada
                         # Mas a chave não bateu com o nó atual
-                        return -1           
+                        return (r, ri)             
+        return (r, ri)
 
-        print("A pesquisa não trata todos os casos!")     
-        return -1
+    def pesquisar(self, chave: str):
+        caminho, letrasProcesadas = self.pesquisaCaminho(chave)
+    
+        if letrasProcesadas < chave.__len__():
+            return -1
+        
+        noAtual = self.raiz
+        for i in range(0, caminho.__len__()):
+            noAtual = noAtual.filhos[caminho[i]]
+
+        if self.maiorPrefixo(chave[::-1], noAtual.chave[::-1]) != noAtual.chave.__len__():
+            return -1
+        
+        return noAtual.valor
+
 
     def inserir(self, chave, valor):
+
         pass
 
     def remover(self, chave, raiz):
