dc362b3645577de17825a557068338ecaeffdbed
mp022 <mpferreiradesouza@gmail.com>
2024-11-15 23:31:27 -0300

    feat: Adição das variáveis e métodos das classes

--- src/trieCompacta.py
+++ src/trieCompacta.py
@@ -1,5 +1,19 @@
 class No:
-    pass
+
+    def __init__(self, chave: str = '-1', valor = -1):
+        self.chave = str(chave)
+        self.valor = valor
+        self.filhos = []
 
 class TrieCompacta:
-    pass
+    def __init__(self) -> None:
+        self.raiz = No()
+
+    def pesquisar(self, chave: str):
+        pass        
+
+    def inserir(self, chave, valor):
+        pass
+
+    def remover(self, chave, raiz):
+        pass
