474ad0c3d7c8c76cf59d7e45f0436d90ba72684a
mp022 <mpferreiradesouza@gmail.com>
2024-11-16 12:09:52 -0300

    fix: Alterações foram feitas na pesquisa para garantir que ela lide com todos casos

--- src/trieCompacta.py
+++ src/trieCompacta.py
@@ -1,5 +1,5 @@
 class No:
-    def __init__(self, chave: str = '-1', valor = -1):
+    def __init__(self, chave = -1, valor = -1):
         self.chave = str(chave)
         self.valor = valor
         self.filhos = []
@@ -25,36 +25,46 @@ class TrieCompacta:
         while noPai.filhos.__len__() > 0 and i < chave.__len__():
             for f in range(0, noPai.filhos.__len__()):
                 noAtual = noPai.filhos[f]
-                maiorPrefixoComum = self.maiorPrefixo(noAtual.chave, chave[i:])
+                # maiorPrefixoComum é menor ou igual a menor chave passada 
+                maiorPrefixoComum = self.maiorPrefixo(str(noAtual.chave), str(chave)[i:])
 
-                # IMPORTANTE: REVISAR SE TODAS POSSIBILIDADE ESTÂO COBERTAS
                 if maiorPrefixoComum == 0:
                     if f+1 == noPai.filhos.__len__():
                         return -1
                     continue
 
-                if chave.__len__() - i < noAtual.chave.__len__() :
+                if  chave.__len__() - i < noAtual.chave.__len__() :
                     if maiorPrefixoComum > 0:
                         return -1
                     continue
-
-                if chave.__len__() - i == noAtual.chave.__len__():
+                elif noAtual.chave.__len__() <  chave.__len__() - i:
                     if noAtual.chave.__len__() == maiorPrefixoComum:
-                        if noAtual.valor == -1:
+                        noPai = noAtual
+                        i += maiorPrefixoComum
+                        break
+                    else:
+                        # Chave do nó já foi toda processada
+                        # Mas não bateu completamente com a chave
+                        return -1
+                else:
+                    if chave.__len__() - i == maiorPrefixoComum:
+                        if noAtual.valor != -1:
+                            return noAtual.valor
+                        else:
                             # Chave já foi toda processada
                             # Verifica se exite nó filho sem chave
                             if (noAtual.filhos.__len__() > 0) and noAtual.filhos[0].chave == '':
+                                # Apenas nós folha possuem valor
                                 return noAtual.filhos[0].valor
                             else:
+                                # Não possui valor no nó encontrado
                                 return -1
-                        return noAtual.valor
                     else:
-                        return -1
-                
-                if noAtual.chave.__len__() == maiorPrefixoComum:
-                    noPai = noAtual
-                    i += maiorPrefixoComum
-                    break
+                        # Chave já foi toda processada
+                        # Mas a chave não bateu com o nó atual
+                        return -1           
+
+        print("A pesquisa não trata todos os casos!")     
         return -1
 
     def inserir(self, chave, valor):
